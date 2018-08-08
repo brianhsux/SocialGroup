d1293bd15d3234d39b991dc2397597dd8cc1149e
Brian Hsu <brianhsux@gmail.com>
2018-08-08 20:18:48 +0800

    add the floating button

---
+++ app/src/main/java/com/brianhsu/socialgroup/Controller/CreatePostActivity.kt
@@ -0,0 +1,13 @@
+package com.brianhsu.socialgroup.Controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.brianhsu.socialgroup.R
+
+class CreatePostActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+    }
+}
